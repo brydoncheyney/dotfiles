a7c9f29fb9244aa80ff28a0641dbd7d8dd15e7e4
Brydon Cheyney <brydon@remission.org.uk>
2017-02-20 23:46:53 +0000

    [setup] allowing init script to be called from anywhere

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${path}/functions
+
 # install prerequisite packages first
-./prereqs/install.sh
+${path}/prereqs/install.sh
 
 while read d; do
-  ls "./${d}install.sh"
+  ls "${path}/${d}install.sh"
   # TODO : link dotfiles
 done <<< "$(ls -d */)"
