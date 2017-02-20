10a3699354735f8a30f96d849926a4074a37748f
Brydon Cheyney <brydon@remission.org.uk>
2017-02-20 23:50:50 +0000

    [setup] ignoring prereqs in install loop

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+# setup environment
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${path}/functions
 
 # install prerequisite packages first
 ${path}/prereqs/install.sh
 
+# install and configure packages
 while read d; do
+  [[ "prereqs/" == "${d}" ]] && continue
   ls "${path}/${d}install.sh"
-  # TODO : link dotfiles
+  # TODO : link dotfiles | backup where required
 done <<< "$(ls -d */)"
