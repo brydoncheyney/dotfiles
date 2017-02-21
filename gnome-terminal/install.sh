202ca6f68ccd2891676b4e6342c77d412864a6b8
Brydon Cheyney <brydon@remission.org.uk>
2017-02-21 22:43:55 +0000

    [gnome-terminal] set default font with dconf

--- gnome-terminal/install.sh
+++ gnome-terminal/install.sh
@@ -4,4 +4,4 @@ set -eou pipefail
 # set default font and size
 profile="/org/gnome/terminal/legacy/profiles:/"
 UUID=$(dconf list ${profile})
-echo dconf write "${profile}${UUID}font" "'Monospace 14'"
+dconf write "${profile}${UUID}font" "'Monospace 14'"
