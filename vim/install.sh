221c6719a7cb7212943e2dbd1c6112ddf122f705
Brydon Cheyney <brydon@remission.org.uk>
2017-02-21 23:51:05 +0000

    [vim] clean up pathogen bundle logging

--- vim/install.sh
+++ vim/install.sh
@@ -14,5 +14,9 @@ for package in ${packages}; do
   dest=$(echo ${package} | awk -F\/ '{print $2}')
   remote=https://github.com/${package}.git;
   [[ ! -d ${dest} ]] && { echo "Installing ${remote}"; git clone ${remote}; }
-  [[ -d ${dest} ]] && { echo "Updating ${remote}"; cd ${dest} && git pull --rebase; cd -; }
+  [[ -d ${dest} ]] && {
+    echo "Updating ${remote}"
+    cd ${dest} && git pull --rebase
+    cd - &> /dev/null
+  }
 done
