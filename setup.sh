1a89b96313f3188e18543301114ac19ad01cf498
Brydon Cheyney <brydon@remission.org.uk>
2017-02-21 22:09:50 +0000

    [setup] refactored script to accept a single argument to install a given package.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-# setup environment
+function usage() {
+  echo "usage: $0 [PACKAGE]"
+  exit 0;
+}
+
+[[ $# -gt 1 ]] && usage
+
+# environment
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${path}/functions
 
-# install prerequisite packages first
-${path}/prereqs/install.sh
+# install prerequisite packages
+hash curl &> /dev/null || sudo apt install -y curl
 
 # install and configure packages
-while read d; do
-  [[ "prereqs/" == "${d}" ]] && continue
+packages=${1:-$(ls -d */ | tr -d '/')}
+while read package; do
+  [[ "prereqs" == "${package}" ]] && continue
 
-  echo "running ${d}install.sh"
-  ${path}/${d}install.sh
+  install_script=${path}/${package}/install.sh
+  [[ -x ${install_script} ]] || {
+    echo "${install_script} does not exist or is not executable - skipping"
+    continue
+  }
+
+  echo "Installing ${package}"
+  ${install_script}
 
   # TODO : link dotfiles | backup where required
-done <<< "$(ls -d */)"
+done <<< "${packages}"
 
 # clean up
 sudo apt autoremove -y
