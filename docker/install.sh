77ac83368af7635fe491008e7984d1e7d69aada1
Brydon Cheyney <brydon@remission.org.uk>
2017-02-23 20:48:47 +0000

    [docker] installing docker-compose

--- docker/install.sh
+++ docker/install.sh
@@ -26,3 +26,7 @@ sudo apt-get install -y docker-engine
 
 # add user to docker group
 sudo usermod -aG docker ${USER}
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
