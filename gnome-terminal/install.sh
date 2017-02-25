2a8dc05b74d4fca8b1a13b37c1468ef49b1e2dd1
Brydon Cheyney <brydon@remission.org.uk>
2017-02-25 13:33:30 +0000

    [gnome-terminal] install only if running X server

--- gnome-terminal/install.sh
+++ gnome-terminal/install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+. ${path}/functions
+
+hash xorg &> /dev/null || { echo "No X server installed... skipping"; exit 0; }
+
+package dconf-cli dconf
+
 # set default font and size
 profile="/org/gnome/terminal/legacy/profiles:/"
 UUID=$(dconf list ${profile})
