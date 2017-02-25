6bd6e2c329d8b31be19a759014f3800c3bc3fe95
Brydon Cheyney <brydon@remission.org.uk>
2017-02-25 13:33:43 +0000

    [google-chrome] install only if running X server

--- google-chrome/install.sh
+++ google-chrome/install.sh
@@ -6,5 +6,7 @@ path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
 exists google-chrome
 
+hash xorg &> /dev/null || { echo "No X server installed... skipping"; exit 0; }
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome.deb
 sudo dpkg -i /tmp/google-chrome.deb
