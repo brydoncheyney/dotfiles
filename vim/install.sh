0a47e10c62113ff7ab2c3a53d8e09749db67bd43
Brydon Cheyney <brydon@remission.org.uk>
2017-03-08 23:38:49 +0000

    [vim] paredit + rainbow parens ftw

--- vim/install.sh
+++ vim/install.sh
@@ -10,12 +10,14 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 packages=(altercation/vim-colors-solarized
           ctrlpvim/ctrlp.vim
           jistr/vim-nerdtree-tabs
+          kien/rainbow_parentheses.vim
           ntpeters/vim-better-whitespace
           scrooloose/nerdtree
           tpope/vim-fugitive
           tpope/vim-sensible
           tpope/vim-surround
           vim-airline/vim-airline
+          vim-scripts/paredit.vim
           vim-scripts/sudo.vim)
 
 cd ~/.vim/bundle
