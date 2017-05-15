e7af5e1befafd6c39a9e4e672918940950dec73c
Brydon Cheyney <brydon@remission.org.uk>
2017-05-15 21:54:54 +0100

    [packer] updating to 1.0.0

--- packer/install.sh
+++ packer/install.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -eu pipefail
 
+version=1.0.0
+
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 . ${path}/functions
 
 exists packer
 
-wget -nc https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_linux_amd64.zip -P /tmp
-wget -nc https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_SHA256SUMS -P /tmp
+wget -nc https://releases.hashicorp.com/packer/${version}/packer_${version}_linux_amd64.zip -P /tmp
+wget -nc https://releases.hashicorp.com/packer/${version}/packer_${version}_SHA256SUMS -P /tmp
 
 cd /tmp
-sha256sum --check --ignore-missing packer_0.12.3_SHA256SUMS \
-  && sudo unzip -d /usr/local/bin/ /tmp/packer_0.12.3_linux_amd64.zip
+sha256sum --check --ignore-missing packer_${version}_SHA256SUMS \
+  && sudo unzip -d /usr/local/bin/ /tmp/packer_${version}_linux_amd64.zip
