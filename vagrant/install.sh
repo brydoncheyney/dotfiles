4e3cc14dd4b79cab7d57ea760d57e95d12781fa5
Brydon Cheyney <brydon@remission.org.uk>
2017-06-05 23:39:58 +0100

    [vagrant] updating to 1.9.5

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -eu pipefail
 
+version=1.9.5
+
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 . ${path}/functions
 
 exists vagrant
 
-wget https://releases.hashicorp.com/vagrant/1.9.2/vagrant_1.9.2_x86_64.deb -P /tmp
-wget https://releases.hashicorp.com/vagrant/1.9.2/vagrant_1.9.2_SHA256SUMS -P /tmp
+wget https://releases.hashicorp.com/vagrant/${version}/vagrant_${version}_x86_64.deb -P /tmp
+wget https://releases.hashicorp.com/vagrant/${version}/vagrant_${version}_SHA256SUMS -P /tmp
 
 cd /tmp
-sha256sum --check --ignore-missing vagrant_1.9.2_SHA256SUMS \
-  && sudo dpkg -i /tmp/vagrant_1.9.2_x86_64.deb
+sha256sum --check --ignore-missing vagrant_${version}_SHA256SUMS \
+  && sudo dpkg -i /tmp/vagrant_${version}_x86_64.deb
 
 for plugin in "vagrant-lxc vagrant-hosts vagrant-triggers"; do
   vagrant plugin install ${plugin}
