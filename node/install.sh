fa2ab9c0658b6d5171c97378e55713d50d666f8d
Brydon Cheyney <brydon@remission.org.uk>
2017-07-30 21:52:24 +0100

    [node] added install script

---
+++ node/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eu pipefail
+
+version=6.11.1
+binary=node-v${version}-linux-x64.tar.xz
+binary_link=https://nodejs.org/dist/v${version}/${binary}
+shasums=SHASUMS256.txt.asc
+shasums_link=https://nodejs.org/dist/v${version}/${shasums}
+
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+. ${path}/functions
+
+exists node
+
+wget -r ${binary_link} -P /tmp
+wget -r ${shasums_link} -P /tmp
+
+(
+cd /tmp
+sha256sum --check --ignore-missing ${shasums} \
+  && { xzcat ${binary} | sudo tar -C/usr/local -xf -; \
+       sudo ln -fs /usr/local/$(basename ${binary} .tar.xz) /usr/local/node;
+  }
+)
