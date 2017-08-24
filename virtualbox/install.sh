60886c044581e4b03b2003950575c98fd092a128
Brydon Cheyney <brydon@remission.org.uk>
2017-08-24 20:00:46 +0100

    [virtualbox] dynamically generate apt source

--- virtualbox/install.sh
+++ virtualbox/install.sh
@@ -6,7 +6,7 @@ path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
 exists virtualbox
 
-sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian yakkety contrib' >> /etc/apt/sources.list"
+sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib' >> /etc/apt/sources.list"
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
