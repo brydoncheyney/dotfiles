5758d463df2add3b1a3774954a96f4441eeaba89
Brydon Cheyney <brydon@remission.org.uk>
2017-09-23 00:29:33 +0100

    [docker] docker-compose 1.16.1

--- docker/install.sh
+++ docker/install.sh
@@ -28,5 +28,5 @@ sudo apt-get install -y docker-engine
 sudo usermod -aG docker ${USER}
 
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
