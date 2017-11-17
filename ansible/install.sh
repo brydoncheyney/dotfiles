557d882cb818c58e4fe8382cfb20144dac41f42a
Brydon Cheyney <brydon@remission.org.uk>
2017-11-17 22:26:42 +0000

    [ansible] passing -y to dependency install

--- ansible/install.sh
+++ ansible/install.sh
@@ -6,7 +6,7 @@ path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
 exists ansible
 
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get -y update
 sudo apt-get -y install ansible
