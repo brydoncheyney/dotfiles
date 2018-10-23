9ff7d3831af22daae2ecb561380e2e6c5a6424fe
Brydon Cheyney <brydon@remission.org.uk>
2018-10-23 22:32:36 +0100

    [vlc] stable repository

--- vlc/install.sh
+++ vlc/install.sh
@@ -6,6 +6,6 @@ path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
 exists vlc
 
-sudo apt-add-repository ppa:videolan/master-daily
+sudo apt-add-repository ppa:videolan/stable-daily
 sudo apt-get -y update
 sudo apt-get -y install vlc browser-plugin-vlc
